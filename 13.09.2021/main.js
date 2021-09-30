e27d737673e4f99c046c063f4603cf4ddbe5e680
unknown <lauralizhg65@gmail.com>
2021-09-30 15:57:24 -0500

    trabajos

---
+++ 13.09.2021/main.js
@@ -0,0 +1,17 @@
+var elementos = [];
+var elementos2 = Array();
+var elementos3 = ["hola",1,8.5,true,5,7,8,9,10];
+var elementos4 = [];
+var palabra = "hola mundo"
+var contenedor = [];
+
+for (var i=0; i<palabra.length;i++){
+    contenedor.push(palabra[i]);
+
+}
+console.log(contenedor)
+
+console.log(contenedor);
+for(let i = 0; i < elementos3.length; i++){
+    console.log(elementos3[i]);
+}//
---
+++ 13.09.2021/tarea13.js
@@ -0,0 +1,18 @@
+var persona = {
+    "Nombres" : "Laura Lizbeth",
+    "Apellidos" : "Hernández Juárez",
+    "Edad" : 18,
+    "Estudios" : {
+        "Nombre universidad" : "UPT",
+        "Carrera" : "Ingeniería en Software",
+        "Cuatrimestre" : 4
+    },
+    "Gustos" : {
+        "Genero de música" : "Ninguno en especifico",
+        "Canción favorita" : "Dime que si- Natos y Waor",
+        "Deporte favorito" : "Futbol",
+        "Epoca favorita del año" : "Día de muertos"
+
+    }
+}
+console.log(persona);
